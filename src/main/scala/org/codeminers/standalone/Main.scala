95b9abb1ddb1009842079a47b2063cfb5ae27d3b
Fabian Yamaguchi <fabs@shiftleft.io>
2022-06-07 19:29:49 +0200

    Create CPG and output list of methods

--- src/main/scala/org/codeminers/standalone/Main.scala
+++ src/main/scala/org/codeminers/standalone/Main.scala
@@ -1,5 +1,22 @@
 package org.codeminers.standalone
 
+import io.joern.javasrc2cpg.{Config, JavaSrc2Cpg}
+import io.joern.x2cpg.X2Cpg.applyDefaultOverlays
+import io.shiftleft.semanticcpg.language._
+
+import scala.util.{Failure, Success}
+
 object Main extends App {
+  val config = Config(inputPaths = Set("testprogram"))
+  JavaSrc2Cpg().createCpg(config) match {
+    case Success(cpg) =>
+      applyDefaultOverlays(cpg)
+
+      println("Printing all methods:")
+      cpg.method.name.foreach(println)
+
+    case Failure(exception) =>
+      println(exception)
+  }
   println("Hello Joern")
 }
